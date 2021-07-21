252da3c286164ef6809f39dcc3e4649682eff5cc
Ron Panduwana <panduwana@gmail.com>
2021-07-21 17:34:12 +0700

    Utilize whitespace to group expressions, using these precedence rules: - Highest precedence: application without whitespace, e.g. `2x` - Then: application using space(s), e.g. `f x` - Then: application using indented block - Last: application using newline(s)

--- parser.rkt
+++ parser.rkt
@@ -1,2 +1,24 @@
 #lang brag
-program : (ID | INT | DECIMAL | STRING | SPACE | NEWLINE | INDENT | DEDENT)+
+
+return : /NEWLINE? expr /NEWLINE?
+
+@expr : expr1
+      | apply2
+
+@expr1 : term 
+       | apply
+       | apply1
+
+apply : term /SPACE (expr|apply)
+
+apply1 : expr /INDENT expr /DEDENT
+
+apply2 : expr1 /NEWLINE (expr|apply2)
+
+@term : INTEGER
+      | DECIMAL
+      | STRING
+      | ID
+      | apply0
+
+apply0 : term term
