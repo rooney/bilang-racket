f440942c006b2f49652caac4f1abec255184ca54
Ron Panduwana <panduwana@gmail.com>
2021-08-02 17:04:22 +0700

    Add tests

---
+++ tests.rkt
@@ -0,0 +1,45 @@
+#lang racket
+(require "parser.rkt" "tokenizer.rkt" brag/support rackunit)
+
+(define (test source result)
+  (check-equal?
+   (parse-to-datum
+    (apply-tokenizer-maker make-tokenizer source))
+   result))
+
+(test #<<EOF
+let scores = [1, 2, 3]
+let avg-score = \
+ scores.reduce() to:0 -> :a:sum :score
+  sum+ score
+ ,/ scores.count()
+println avg-score
+EOF
+      '(return
+        (apply3
+         (applyL
+          let
+          (applyL
+           scores
+           (applyL = (group (applyK (applyK (apply0 bracket 1) 2) 3)))))
+         (apply3
+          (applyL
+           let
+           (applyL
+            avg-score
+            (applyL
+             =
+             (applyK
+              (commaOP
+               (applyZ
+                (apply0 (apply0 scores (dot reduce)) (group paren))
+                (applyZ
+                 (applyO (keyword to :) 0)
+                 (applyZ
+                  ->
+                  (apply2
+                   (apply1 (alias (label : a) (label : sum)) (label : score))
+                   (apply1 (applyO sum +) score)))))
+               /)
+              (apply0 (apply0 scores (dot count)) (group paren))))))
+          (apply1 println avg-score)))))
